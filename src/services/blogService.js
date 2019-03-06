5d2685be6d05b82ab176c140818f2a4a5c4e14ea
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-06 13:52:17 -0800

    changed name

---
+++ src/services/blogService.js
@@ -0,0 +1,14 @@
+export const getPosts = () => {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json());
+};
+
+export const getComments = (postId) => {
+  return fetch(`https://jsonplaceholder.typicode.com/comments/${postId}`)
+    .then(res => res.json());
+};
+
+export const getUsers = () => {
+  return fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json());
+};

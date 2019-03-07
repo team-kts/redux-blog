ddc4c700044fee75d1da7e845753b570ab108889
Sophie Graham <sophiergraham1@gmail.com>
2019-03-07 13:01:08 -0800

    passing second user reducer test

--- src/reducers/users.test.js
+++ src/reducers/users.test.js
@@ -60,5 +60,62 @@ describe('users reducer', () => {
       }]
     });
   });
-});
 
+  it('handles the fetch user by id action', () => {
+    const state = {
+      user: {}
+    };
+    const fetchedUser = reducer(state, {
+      type: 'FETCH_USER',
+      payload: {
+        'id': 1,
+        'name': 'Leanne Graham',
+        'username': 'Bret',
+        'email': 'Sincere@april.biz',
+        'address': {
+          'street': 'Kulas Light',
+          'suite': 'Apt. 556',
+          'city': 'Gwenborough',
+          'zipcode': '92998-3874',
+          'geo': {
+            'lat': '-37.3159',
+            'lng': '81.1496'
+          }
+        },
+        'phone': '1-770-736-8031 x56442',
+        'website': 'hildegard.org',
+        'company': {
+          'name': 'Romaguera-Crona',
+          'catchPhrase': 'Multi-layered client-server neural-net',
+          'bs': 'harness real-time e-markets'
+        }
+      }
+    });
+
+    expect(fetchedUser).toEqual({
+      user: {
+        'id': 1,
+        'name': 'Leanne Graham',
+        'username': 'Bret',
+        'email': 'Sincere@april.biz',
+        'address': {
+          'street': 'Kulas Light',
+          'suite': 'Apt. 556',
+          'city': 'Gwenborough',
+          'zipcode': '92998-3874',
+          'geo': {
+            'lat': '-37.3159',
+            'lng': '81.1496'
+          }
+        },
+        'phone': '1-770-736-8031 x56442',
+        'website': 'hildegard.org',
+        'company': {
+          'name': 'Romaguera-Crona',
+          'catchPhrase': 'Multi-layered client-server neural-net',
+          'bs': 'harness real-time e-markets'
+        }
+      }
+    });
+  });
+});

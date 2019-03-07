e34e87e75fb540faeff10f83fb7c5aa645ba2f2a
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-07 09:36:03 -0800

    passing second post reducer test

--- src/reducers/posts.test.js
+++ src/reducers/posts.test.js
@@ -29,19 +29,30 @@ describe('posts reducer', () => {
     });
   });
 
-  // it('handles the update markdown body action', () => {
-  //   const state = {
-  //     title: '',
-  //     body: ''
-  //   };
-  //   const updatedMarkdown = reducer(state, {
-  //     type: 'UPDATE_MARKDOWN_BODY',
-  //     payload: 'newbody'
-  //   });
+  it('handles the fetch post action', () => {
+    const state = {
+      posts: [],
+      post: {}
+    };
+    const fetchedPost = reducer(state, {
+      type: 'FETCH_POST',
+      payload: 
+        {
+          'userId': 1,
+          'id': 1,
+          'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+          'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+        }
+    });
 
-  //   expect(updatedMarkdown).toEqual({
-  //     title: '', 
-  //     body: 'newbody'
-  //   });
-  // });
+    expect(fetchedPost).toEqual({
+      post: {
+        'id': 1,
+        'userId': 1,
+        'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+        'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+      },
+      posts: []
+    });
+  });
 });

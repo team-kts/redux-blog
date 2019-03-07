ff3bbf7ba24e368273b000df7876e8ba9f301678
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-07 09:21:48 -0800

    passing fetchUsers actions test

---
+++ src/actions/users.test.js
@@ -0,0 +1,18 @@
+import { FETCH_USERS, fetchUsers } from './users';
+import { getUsers } from '../services/blogService';
+
+jest.mock('../services/blogService');
+
+describe('actions test', () => {
+  it('fetches posts', () => {
+    const action = fetchUsers();
+
+    expect(action).toEqual({ 
+      type: FETCH_USERS,
+      loadStart: 'LOAD_USERS_START',
+      loadEnd: 'LOAD_USERS_END',
+      payload:  getUsers()
+    });
+  });
+});
+

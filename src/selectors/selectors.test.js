7b88c5bcc1755d7b318551346b2c60b411d9c6f7
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-07 09:12:41 -0800

    passing getUsers selectors test

--- src/selectors/selectors.test.js
+++ src/selectors/selectors.test.js
@@ -1,4 +1,4 @@
-import { getPosts } from './blog';
+import { getPosts, getUsers } from './blog';
 
 describe('selectors test', () => {
   it('can get posts', () => {
@@ -26,6 +26,55 @@ describe('selectors test', () => {
         'id': 1,
         'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
         'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+      }
+    ]);
+  });
+
+  it('can get users', () => {
+    const state = {
+      users: {
+        users: [{
+          'id': 1,
+          'name': 'Leanne Graham',
+          'username': 'Bret',
+          'email': 'Sincere@april.biz',
+          'address': {
+            'street': 'Kulas Light',
+            'suite': 'Apt. 556',
+            'city': 'Gwenborough',
+            'zipcode': '92998-3874',
+            'geo': {
+              'lat': '-37.3159',
+              'lng': '81.1496'
+            }
+          }
+        }]
+      },
+      comments: [],
+      posts: {
+        posts: [],
+        post: {}
+      }
+    };
+
+    const fetchedUsers = getUsers(state);
+      
+    expect(fetchedUsers).toEqual([
+      {
+        'id': 1,
+        'name': 'Leanne Graham',
+        'username': 'Bret',
+        'email': 'Sincere@april.biz',
+        'address': {
+          'street': 'Kulas Light',
+          'suite': 'Apt. 556',
+          'city': 'Gwenborough',
+          'zipcode': '92998-3874',
+          'geo': {
+            'lat': '-37.3159',
+            'lng': '81.1496'
+          }
+        }
       }
     ]);
   });

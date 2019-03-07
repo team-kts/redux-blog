9b7bfa63b8d7834594580f40e0455e24f330869e
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-07 11:08:09 -0800

    getuserbyid passing test

--- src/actions/users.test.js
+++ src/actions/users.test.js
@@ -1,5 +1,5 @@
-import { FETCH_USERS, fetchUsers } from './users';
-import { getUsers } from '../services/blogService';
+import { FETCH_USERS, fetchUsers, FETCH_USER, fetchUserById } from './users';
+import { getUsers, getUserById } from '../services/blogService';
 
 jest.mock('../services/blogService');
 
@@ -12,6 +12,17 @@ describe('actions test', () => {
       loadStart: 'LOAD_USERS_START',
       loadEnd: 'LOAD_USERS_END',
       payload:  getUsers()
+    });
+  });
+
+  it('fetches users by id', () => {
+    const action = fetchUserById(1);
+
+    expect(action).toEqual({ 
+      type: FETCH_USER,
+      loadStart: 'LOAD_USER_START',
+      loadEnd: 'LOAD_USER_END',
+      payload:  getUserById(1)
     });
   });
 });
